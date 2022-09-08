d69dd3d64793e0c79fb7eeb9e12a724c638b2568
Blessneha <91933800+Blessneha@users.noreply.github.com>
2022-09-08 15:41:00 +0530

    Add files via upload

---
+++ ConsoleApp8/ConsoleApp8/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace ConsoleApp8
+{
+    internal class Program
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=LAPTOP-45DGQP0R\SQLEXPRESS;Initial Catalog=Adoassign1;Integrated Security=True");
+        SqlCommand cmd = null;
+
+
+
+        public void AddProduct(string name, DateTime dob, string hdeg, string jobtitle, string company, string email) //add product
+        {
+            try
+            {
+
+                string query = $"insert into student2 values('{name}','{dob}','{hdeg}','{jobtitle}','{company}','{email}')";
+                cmd = new SqlCommand(query, con);
+                con.Open();
+                cmd.ExecuteNonQuery();
+
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public void GetProductByName(string name) //get product by Id
+        {
+            try
+            {
+                string query = $"Select name,dob,hdeg,jobtitle,company,email from student2 where name='{name}'";
+                cmd = new SqlCommand(query, con);
+                con.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+                if (dr.HasRows)
+                {
+                    dr.Read();
+                    Console.WriteLine("Name:{0} DOB:{1} degree:{2} job{3} company{4} email{5}", dr["name"], dr["dob"], dr["hdeg"], dr["jobtitle"], dr["company"], dr["email"]);
+
+                }
+                else
+                    Console.WriteLine("Invalid name");
+
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            // Program p=new Program();
+            // p.AddProduct("Blessy",DateTime.Now, "ff", "ere", "wwe", "ergg");
+            //   p.GetProductByName("Blessy");
+            Q2 q = new Q2();
+            //  q.addmovie(1, "rtt", 2000, "ccc", "xxxx");
+            //     q.GetMoviesByName("rtt");
+            // q.GetallMovies();
+            //q.addmovie(2, "blessy", 2001,"bbb","yyyy","reszona");
+
+            //  q.addmovie(3, "furniture", 2002, "hhjmm", "ekelw","hazel");
+            //q.UpdateProduct(1, 2003, "abc", "wers");
+            // q.DeleteProduct(3);
+            // q.GetAllMoviesByActor("hazel");
+            q.GetAllMoviesByDirector("yyyy");
+        }
+    }
+}
+
+       
+
---
+++ ConsoleApp8/ConsoleApp8/Q2.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ConsoleApp8
+{
+    internal class Q2
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=LAPTOP-45DGQP0R\SQLEXPRESS;Initial Catalog=Adoassign1;Integrated Security=True");
+        SqlCommand cmd = null;
+        SqlDataAdapter da = null;
+        DataSet ds = null;
+
+        public void addmovie(int MovieId,string Name,int year,string Lang,string Director,string Actor)
+        {
+            try
+            {
+
+                string query = $"insert into Movie values({MovieId},'{Name}',{year},'{Lang}','{Director}','{Actor}')";
+                cmd = new SqlCommand(query, con);
+                con.Open();
+                cmd.ExecuteNonQuery();
+               
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+
+        }
+
+        public void GetMoviesByName(string name) //get product by Id
+        {
+            
+            try
+            {
+                string query = $"Select MovieId,Name,year,Lang,Director from Movie where Name='{name}'";
+                cmd = new SqlCommand(query, con);
+                con.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+                if (dr.HasRows)
+                {
+                    dr.Read();
+                    Console.WriteLine("MovieId:{0} ,Name:{1} ,year:{2}, Lang{3}, Director{4},Actor{5}",dr["MovieId"],dr["Name"],dr["year"],dr["Lang"],dr["Director"],dr["Actor"]);
+
+                }
+                else
+                    Console.WriteLine("Invalid name");
+
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+        public void GetallMovies()
+        {
+            try
+            {
+                string query = "Select * from Movie";
+                cmd = new SqlCommand(query, con);
+                con.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+
+                if (dr.HasRows)
+                {
+                    while (dr.Read())
+                    {
+                        Console.WriteLine("MovieId:{0} ,Name:{1} ,year:{2}, Lang{3}, Director{4},Actor{5}", dr["MovieId"], dr["Name"], dr["year"], dr["Lang"], dr["Director"], dr["Actor"]);
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        public void DeleteProduct(int MovieId) //delete product
+        {
+            try
+            {
+                
+                    string query= $"Delete from Movie where MovieId={MovieId}";
+                    SqlCommand cmd = new SqlCommand(query,con);
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+        public void UpdateProduct(int MovieId, int year,string Actor,string Director) //update product 
+        {
+            try
+            {
+                string query = $"Update Movie set year={year},Actor='{Actor}',Director='{Director}' Where MovieId={MovieId}";
+                    SqlCommand cmd = new SqlCommand(query,con);
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        public void GetAllMoviesByActor(string actor)
+        {
+            
+            string qry = $"Select MovieId,Name,year,Lang,Director,Actor from Movie where Actor='{actor}'";
+            da = new SqlDataAdapter(qry, con);
+            ds = new DataSet();
+            da.Fill(ds, "Movie");
+            Console.WriteLine("Movie records");
+            foreach (DataRow row in ds.Tables["Movie"].Rows)
+            {
+                Console.WriteLine("MovieId:{0} ,Name:{1} , year:{2}, Lang:{3} , Director:{4},Actor:{5}",row["MovieId"],row["Name"],row["year"],row["Lang"],row["Director"],row["Actor"]);
+            }
+            
+        }
+
+        public void GetAllMoviesByDirector(string director)
+        {
+
+            string qry = $"Select MovieId,Name,year,Lang,Director,Actor from Movie where Director='{director}'";
+            da = new SqlDataAdapter(qry, con);
+            ds = new DataSet();
+            da.Fill(ds, "Movie");
+            Console.WriteLine("Movie records");
+            foreach (DataRow row in ds.Tables["Movie"].Rows)
+            {
+                Console.WriteLine("MovieId:{0} ,Name:{1} , year:{2}, Lang:{3} , Director:{4},Actor:{5}", row["MovieId"], row["Name"], row["year"], row["Lang"], row["Director"], row["Actor"]);
+            }
+
+        }
+    }
+}
